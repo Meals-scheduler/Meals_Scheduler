823fd950da69b56ce1c59b577dc61f60823434ba
Elad Weinbrand <eladweinbrand@Elads-iMac.local>
2021-03-02 07:32:12 -0800

    again

---
+++ Test copy/shared/src/commonMain/kotlin/com/example/test/shared/Greeting.kt
@@ -0,0 +1,9 @@
+package com.example.test.shared
+
+
+class Greeting {
+    fun greeting(): String {
+        //return "Hello, ${Platform().platform}!"
+        return "Elad1236666"
+    }
+}
---
+++ Test copy/shared/src/commonMain/kotlin/com/example/test/shared/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.test.shared
+
+expect class Platform() {
+    val platform: String
+}
